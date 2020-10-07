88236be41286ed0736d428f5b0dcf48931a5fb75
mattlaw <matt@piximo.me>
2020-10-07 21:26:21 +0100

    first commit

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+
+from . import app
+
+# Replace the existing home function with the one below
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
